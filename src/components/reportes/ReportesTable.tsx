226cfd11f8db50795cec3c58c9f378d40f7cdf1c
Rommel <rommel.xana@gmail.com>
2025-04-02 10:50:46 +0200

    maquetacion_fichajes

--- src/components/reportes/ReportesTable.tsx
+++ src/components/reportes/ReportesTable.tsx
@@ -1,9 +1,18 @@
 import styles from './reportesTable.module.css'
+import ReportesTableItem from './ReportesTableItem';
 
 export default function ReportesTable() {
   return (
     <div className={styles.table}>
-      <div className={styles.tableHEader}></div>
+      <div className={styles.tableHeader}>
+        <h2>Usuarios</h2>
+        <h2>Horas semanales</h2>
+        <div className={styles.empty}></div>
+      </div>
+      <ReportesTableItem />
+      <ReportesTableItem />
+      <ReportesTableItem />
+      <ReportesTableItem />
     </div>
   );
 }
