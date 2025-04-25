36055095946199e68d120f9683d0f9dc23501e17
Rommel <rommel.xana@gmail.com>
2025-04-25 10:40:48 +0200

    fix bug nav

--- src/app/dashboard/perfil/solicitudes/page.tsx
+++ src/app/dashboard/perfil/solicitudes/page.tsx
@@ -1,10 +1,28 @@
+'use client'
+
 import ContainerOptions from '@/components/containers/ContainerOptions';
 import EntradaSolicitudes from '@/components/containers/historialSolicitudes/EntradaSolicitudes';
+import { useState } from 'react';
 
 export default function Solicitudes() {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [option, setOption] = useState('Esta semana');
+  const [localizacion, setLocalizacion] = useState('all');
   return (
     <>
-      <ContainerOptions ubicacion={false} urlExportar={'#'} usuarios={false} añadirUsuario={false} />
+      <ContainerOptions ubicacion={false}
+        urlExportar={'#'}
+        usuarios={false}
+        añadirUsuario={false}
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        option={option}
+        setOption={setOption}
+        localizacion={localizacion}
+        setLocalizacion={setLocalizacion} />
       <EntradaSolicitudes />
       <EntradaSolicitudes />
       <EntradaSolicitudes />
--- src/app/dashboard/recursos/reportes/page.tsx
+++ src/app/dashboard/recursos/reportes/page.tsx
@@ -1,148 +1,162 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import ContainerOptions from '@/components/containers/ContainerOptions';
-import styles from './reportes.module.css'
+import styles from './reportes.module.css';
 import ReportesTable from '@/components/recursos/reportes/ReportesTable';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
 
 type UserData = {
   id: string,
   nombre: string,
   apellido: string,
-  email: string
+  email: string,
   image: string,
   horas_semanales: string,
   horas_restantes: string
-}
+};
 
-export default async function ReportesPage() {
-  const date = new Date();
+export default function ReportesPage() {
+  const [usersData, setUsersData] = useState<UserData[]>([]);
+  const [totalHorasTrabajadas, setTotalHorasTrabajadas] = useState<string>('00:00');
+  const router = useRouter();
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [option, setOption] = useState('Esta semana');
+  const [localizacion, setLocalizacion] = useState('all');
 
-  const startOfWeek = new Date(date);
-  const day = startOfWeek.getDay();
-  const diffToMonday = day === 0 ? -6 : 1 - day;
-  startOfWeek.setDate(startOfWeek.getDate() + diffToMonday);
-  startOfWeek.setHours(0, 0, 0, 0);
+  useEffect(() => {
+    const fetchData = async () => {
+      const supabase = createClient();
+      const { data } = await supabase.auth.getUser();
+      const user = data.user;
 
-  const endOfWeek = new Date(startOfWeek);
-  endOfWeek.setDate(startOfWeek.getDate() + 5);
-  endOfWeek.setHours(0, 0, 0, 0)
+      if (!user) {
+        router.push('/login');
+        return;
+      }
 
-  const supabase = await createClient();
-  const { data } = await supabase.auth.getUser();
-  const user = data.user;
+      const { data: dataProfile, error: errorProfile } = await supabase
+        .from('profiles')
+        .select('*')
+        .neq('user_id', user.id);
 
-  if (!user) {
-    redirect('/login')
-  }
+      if (errorProfile) {
+        console.error('Error fetching profiles: ', errorProfile);
+        return;
+      }
 
-  const { data: dataProfile, error: errorProfile } = await supabase
-    .from('profiles')
-    .select('*')
-    .neq('user_id', user.id);
+      if (!dataProfile || dataProfile.length === 0) {
+        await supabase.auth.signOut();
+        router.push('/login');
+        return;
+      }
 
-  if (errorProfile) {
-    console.log('Error fetching profiles: ', errorProfile);
-  }
+      const date = new Date();
+      const startOfWeek = new Date(date);
+      const day = startOfWeek.getDay();
+      const diffToMonday = day === 0 ? -6 : 1 - day;
+      startOfWeek.setDate(startOfWeek.getDate() + diffToMonday);
+      startOfWeek.setHours(0, 0, 0, 0);
 
-  let profiles = [];
+      const endOfWeek = new Date(startOfWeek);
+      endOfWeek.setDate(startOfWeek.getDate() + 5);
+      endOfWeek.setHours(0, 0, 0, 0);
 
-  if (dataProfile && dataProfile.length > 0) {
-    profiles = dataProfile && dataProfile.length > 0 ? dataProfile : [];
-  } else {
-    await supabase.auth.signOut();
-    redirect('/login')
-  }
+      let totalHoras = 0;
+      const users: UserData[] = [];
 
-  const usersData: UserData[] = [];
-  let totalHorasTrabajadas = 0;
+      for (const profile of dataProfile) {
+        const { data: fichajeJornada } = await supabase
+          .from('fichaje_jornada')
+          .select('*')
+          .eq('profile_id', profile.id)
+          .gte('date', startOfWeek.toISOString())
+          .lt('date', endOfWeek.toISOString());
 
-  for (let i = 0; i < profiles.length; i++) {
-    const profile = profiles[i];
+        let totalHorasNetas = 0;
 
-    const { data: fichajeJornada, error: errorFichajeJornada } = await supabase
-      .from('fichaje_jornada')
-      .select('*')
-      .eq('profile_id', profile.id)
-      .gte('date', startOfWeek.toISOString())
-      .lt('date', endOfWeek.toISOString());
+        if (fichajeJornada && fichajeJornada.length > 0) {
+          for (const jornada of fichajeJornada) {
+            const { data: eventos } = await supabase
+              .from('fichaje_eventos')
+              .select('evento, date')
+              .eq('fichaje_id', jornada.id)
+              .order('date', { ascending: true });
 
-    if (errorFichajeJornada) {
-      console.log('Error fetching Fichajes Jornada: ', errorFichajeJornada);
-    }
+            if (eventos && eventos.length > 0) {
+              let inicioJornada: Date | null = null;
+              let finJornada: Date | null = null;
+              let pausaInicio: Date | null = null;
+              let totalPausas = 0;
 
-    let totalHorasNetas = 0;
+              for (const evento of eventos) {
+                const hora = new Date(evento.date);
 
-    if (fichajeJornada && fichajeJornada.length > 0) {
-      for (const jornada of fichajeJornada) {
-        const { data: eventos, error: errorEventos } = await supabase
-          .from('fichaje_eventos')
-          .select('evento, date')
-          .eq('fichaje_id', jornada.id)
-          .order('date', { ascending: true });
+                if (evento.evento === 'Inicio Jornada' && !inicioJornada) {
+                  inicioJornada = hora;
+                } else if (evento.evento === 'Jornada Finalizada') {
+                  finJornada = hora;
+                } else if (evento.evento === 'Inicio Pausa') {
+                  pausaInicio = hora;
+                } else if (evento.evento === 'Final Pausa' && pausaInicio && hora) {
+                  totalPausas += (hora.getTime() - pausaInicio.getTime()) / 1000 / 60 / 60;
+                  pausaInicio = null;
+                }
+              }
 
-        if (errorEventos) {
-          console.log('Error fetching Eventos: ', errorEventos);
-        }
-
-        if (eventos && eventos.length > 0) {
-          let inicioJornada = null;
-          let finJornada = null;
-          let pausaInicio = null;
-          let totalPausas = 0;
-
-          for (const evento of eventos) {
-            const hora = new Date(evento.date);
-
-            if (evento.evento === 'Inicio Jornada' && !inicioJornada) {
-              inicioJornada = hora;
-            } else if (evento.evento === 'Jornada Finalizada') {
-              finJornada = hora;
-            } else if (evento.evento === 'Inicio Pausa') {
-              pausaInicio = hora;
-            } else if (evento.evento === 'Final Pausa' && pausaInicio instanceof Date && hora instanceof Date) {
-              totalPausas += (hora.getTime() - pausaInicio.getTime()) / 1000 / 60 / 60;
-              pausaInicio = null;
+              if (inicioJornada && finJornada && finJornada > inicioJornada) {
+                const duracionJornada = (finJornada.getTime() - inicioJornada.getTime()) / 1000 / 60 / 60;
+                const horasNetas = duracionJornada - totalPausas;
+                totalHorasNetas += horasNetas;
+                totalHoras += horasNetas;
+              }
             }
           }
-
-          if (inicioJornada instanceof Date && finJornada instanceof Date && finJornada > inicioJornada) {
-            const duracionJornada = (finJornada.getTime() - inicioJornada.getTime()) / 1000 / 60 / 60;
-            const horasNetas = duracionJornada - totalPausas;
-            totalHorasNetas += horasNetas;
-            totalHorasTrabajadas += horasNetas;
-          }
         }
+
+        const horasRestantes = profile.horas_semana - totalHorasNetas;
+
+        users.push({
+          id: profile.id,
+          nombre: profile.nombre,
+          apellido: profile.apellido,
+          email: profile.email,
+          image: profile.image,
+          horas_semanales: formatHoras(profile.horas_semana),
+          horas_restantes: formatHoras(parseFloat(horasRestantes.toFixed(2)))
+        });
       }
-    }
 
-    const horasRestantes = profile.horas_semana - totalHorasNetas;
+      setUsersData(users);
+      setTotalHorasTrabajadas(formatHoras(totalHoras));
+    };
 
-    usersData.push({
-      id: profile.id,
-      nombre: profile.nombre,
-      apellido: profile.apellido,
-      email: profile.email,
-      image: profile.image,
-      horas_semanales: formatHoras(profile.horas_semana),
-      horas_restantes: formatHoras(parseFloat(horasRestantes.toFixed(2)))
-    });
-  }
+    fetchData();
+  }, []);
 
   function formatHoras(horasDecimales: number): string {
-    const horas = Math.floor(horasDecimales);  
+    const horas = Math.floor(horasDecimales);
     const minutos = Math.round((horasDecimales - horas) * 60);
     return `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}`;
   }
-  
-
-
-
-
 
   return (
     <div className={styles.container}>
-      <ContainerOptions ubicacion={false} tipoRegistro={false} urlExportar='#' recientes={false} añadirUsuario={false} />
-      <ReportesTable users={usersData} totalHorasTrabajadas={formatHoras(totalHorasTrabajadas)} />
+      <ContainerOptions ubicacion={false}
+        urlExportar={'#'}
+        usuarios={false}
+        añadirUsuario={false}
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        option={option}
+        setOption={setOption}
+        localizacion={localizacion}
+        setLocalizacion={setLocalizacion} />
+      <ReportesTable users={usersData} totalHorasTrabajadas={totalHorasTrabajadas} />
     </div>
   );
 }
